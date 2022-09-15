81ac561c5384dca68680bae67b0ab51c85828d51
Abdullah-Niaz <abdullahniaz423@gmail.com>
2022-09-15 11:35:13 +0500

    strings

--- strings/intro-strings.cpp
+++ strings/intro-strings.cpp
@@ -2,8 +2,12 @@
 using namespace std;
 
 int main() {
-    cout<<"hello ";
-    cout<<"world";
+    char x ='A'; // character we can store only one char in char type.
+    char y[] ="Hello World" ;// in order to store the strings, we need to declare the array in char type
+    // in order to store the string we use double quotes
+    // Details ['H','e','l','l','o','w','o','r','l','d','\o' > this is a string terminator] 
+    cout<<x<<endl;
+    cout<<y;
     return 0;
 }
  
