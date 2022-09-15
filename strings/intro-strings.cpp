1e4de5cf0c9585904a17107c78735ffe3c0d82f4
Abdullah-Niaz <abdullahniaz423@gmail.com>
2022-09-15 12:22:44 +0500

    strings

--- strings/intro-strings.cpp
+++ strings/intro-strings.cpp
@@ -6,6 +6,7 @@ int main() {
     char y[] ="Hello World" ;// in order to store the strings, we need to declare the array in char type
     // in order to store the string we use double quotes
     // Details ['H','e','l','l','o','w','o','r','l','d','\o' > this is a string terminator] 
+    // Details ['H','e','l','l','o','w','o','r','l','d','\o' > this is a string terminator] 
     cout<<x<<endl;
     cout<<y;
     return 0;
