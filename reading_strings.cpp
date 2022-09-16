1c1a820fffe454bb0721a23cc15ed2a839c4d9cf
Abdullah-Niaz <abdullahniaz423@gmail.com>
2022-09-16 11:50:44 +0500

    reading inputs

---
+++ reading_strings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char s[20];
+    cout<<"Enter the your name ";
+    cin>>s;
+    // cin.get(s,20);
+    // cin.getline(s,20)
+    cout<<"Wellcome "<< s ;
+};
--- strings/intro-strings.cpp
+++ strings/intro-strings.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    cout<<"Hello World";
-    char x ='A'; // character we can store only one char in char type.
-    char y[] ="Hello World" ;// in order to store the strings, we need to declare the array in char type
+    // cout<<"Hello World";
+    // char x ='A'; // character we can store only one char in char type.
+    // char y[] ="Hello World" ;// in order to store the strings, we need to declare the array in char type
     // in order to store the string we use double quotes
     // Details ['H','e','l','l','o','w','o','r','l','d','\o' > this is a string terminator] 
     // Details ['H','e','l','l','o','w','o','r','l','d','\o' > this is a string terminator] 
+    // cout<<x<<endl;
+    // cout<<y<<endl;
+
+    // Getting ascii code from the numbers
+    char x[] = {65,66,67,68,69};
     cout<<x<<endl;
-    cout<<y;
+
+    // char s[] = {'H','e','l','l','o','w','o','r','l','d','\0'};
+    // cout<<s<<endl;
+
+
+    // Create a string using pointers
+    // char *s = "Hello Worl";
+    // cout<<s;
     return 0;
 }
  
