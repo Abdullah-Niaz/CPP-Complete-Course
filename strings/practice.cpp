deb884944bb295185a9ab90d0ffc5997e7a3b703
Abdullah-Niaz <abdullahniaz423@gmail.com>
2022-09-26 09:08:56 +0500

    string concat and taking input from the user and counting the lenght of the user

---
+++ strings/practice.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main(){
+    // taking input from user using the get and getline method 
+    // char i[20] ;
+    // cin.get(i,20);
+    // cout<<"Here is your Character " << i <<endl;
+
+    // Taking input from the user and count the lenght of the our char 
+    // char x[100] ;
+    // cin.getline(x,100);
+    // cout<<"The lenght of your string is "<<strlen(x);
+
+    // // concatination
+    char x1[] = "Abdullah! ";
+    char x2[] = " I am a developer";
+    // cout<< x1 + x2;
+    // string concatinatino function to add the string to the other variable.
+    // cout<<strcat(x2,x1)<<endl;
+    // string according to the specific values 
+    // strncat(destination, source, lenght )
+    strncat(x2, x1 , 5);
+
+    cout<<x2;
+
+
+
+    return 0;
+}
