c73e8a2d4955cb62e35a9f27c6c31e1a7e344d3c
Abdullah-Niaz <abdullahniaz423@gmail.com>
2022-10-10 03:38:30 +0500

    string concatenations

--- strings/class_strings.cpp
+++ strings/class_strings.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <string>
+#include <string.h>
 using namespace std;
 int main(){
     
-    string  str = "Hello World";
+    // string  str = "Hello World";
     
     // getline(cin,str);
     // cout<<str.max_size();
@@ -13,12 +14,18 @@ int main(){
     // cout<<str.length();
     // str.clear();
     // cout<<str;
-    str.empty();
-    if (str.empty()){
-        cout<<"String is empty";
-    }
-    else {
-        cout<<"String is not empty and value is "<<str<<endl;
-    }
+    // str.empty();
+    // if (str.empty()){
+    //     cout<<"String is empty";
+    // }
+    // else {
+    //     cout<<"String is not empty and value is "<<str<<endl;
+    // }
+
+    string str1 = "programming";
+    string str2 = " is most popular among every profession";
+    string str3;
+    str3 = str1 + str2;
+    cout<<str3;
     return 0; 
 }
