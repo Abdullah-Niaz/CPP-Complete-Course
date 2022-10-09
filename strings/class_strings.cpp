010c32690cf772f96effec1f8c802d927b72cb2c
Abdullah-Niaz <abdullahniaz423@gmail.com>
2022-10-10 03:52:16 +0500

    accessing the string using the begin() and end() function from start

--- strings/class_strings.cpp
+++ strings/class_strings.cpp
@@ -28,12 +28,23 @@ int main(){
     // str3 = str1 + str2;
     // cout<<str3;
     string str = "hello";
-    accesing the string using the for loop
-    for(int i=0; str[i] != '\0';i++){
-        // cout<<str[i] ;
-        // Another way to acces it in uppar case 
-        str[i] = str[i] - 32;
+    // // accesing the string using the for loop
+    // for(int i=0; str[i] != '\0';i++){
+    //     // cout<<str[i] ;
+    //     // Another way to acces it in uppar case 
+    //     str[i] = str[i] - 32;
+    // }
+    // cout<<str;
+
+    // Another way of accesing is using the functions 
+    // Acessing from the start
+    string::iterator it;
+    for(it= str.begin(); it != str.end();it++){
+        cout<<*it;
+        
     }
-    cout<<str;
+    
+    // Accessing from reverse
+
     return 0; 
 }
