04fd9efebf606c7e84d17f0b6b5a60bbe06c8698
Abdulah-Niaz <abdullahniaz423@gmail.com>
2023-02-12 18:56:23 +0500

    Removed Extra files

---
+++ 3. Loops/For Loop/For_Loop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Foor loop syntax
+    // for (initilization ; Condition;  Update)
+    // {
+    //     Body of the Loop
+    // }
+
+    // Print the 100 Number with message
+    for (int i = 0; i <= 100; i++)
+    {
+        cout << "This is my message no: " << i << endl;
+    }
+
+    return 0;
+}
--- C++ Basics/tempCodeRunnerFile.cpp
+++
@@ -1,4 +0,0 @@
-for (int i = 0; i < 20;  i++)
-    // {
-    //     cout<<i <<endl;
-    // }
