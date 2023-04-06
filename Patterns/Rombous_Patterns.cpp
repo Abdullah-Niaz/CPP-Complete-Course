e472c9004a1178ed68a9523a603625837fb472d4
Abdullah-Niaz <abdullahniaz423@gmail.com>
2023-04-07 01:13:42 +0500

    Rombous Patters

---
+++ Patterns/Rombous_Patterns.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int row;
+    cout << "Enter your rows: ";
+    cin >> row;
+    for (int i = 1; i <= row; i++)
+    {
+        for (int j = 1; j <= row - i; j++)
+        {
+            cout << " & ";
+        }
+        for (int j = 1; j <=row; j++)
+        {
+            cout<<" * ";
+        }
+        cout << endl;
+        
+    }
--- Patterns/dumy.cpp
+++ Patterns/dumy.cpp
@@ -1,21 +1,44 @@
 #include <iostream>
 using namespace std;
-int main(){
-
+int main()
+{
     int row;
-    cout<<"Enter the Row: ";
-    cin>>row;
+    cout << "Enter your rows: ";
+    cin >> row;
     for (int i = 1; i <= row; i++)
     {
-        for (int j = 1; j <= row-i; j++){
-            cout<<" - ";
+        for (int j = 1; j <= row - i; j++)
+        {
+            cout << " & ";
         }
-        for (int j = 1; j <= row; j++)
+        for (int j = 1; j <=row; j++)
         {
             cout<<" * ";
         }
+        cout << endl;
         
-        cout<<endl;
     }
-    
-}
+
+} // namespace std;
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+
+//     int row;
+//     cout<<"Enter the Row: ";
+//     cin>>row;
+//     for (int i = 1; i <= row; i++)
+//     {
+//         for (int j = 1; j <= row-i; j++){
+//             cout<<" - ";
+//         }
+//         for (int j = 1; j <= row; j++)
+//         {
+//             cout<<" * ";
+//         }
+
+//         cout<<endl;
+//     }
+
+// }
