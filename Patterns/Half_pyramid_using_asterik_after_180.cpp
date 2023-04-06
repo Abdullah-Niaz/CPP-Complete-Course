482c244e8b2a55ea94a4eb68440a20480e2ddcae
Abdullah-Niaz <abdullahniaz423@gmail.com>
2023-04-06 23:27:46 +0500

    Half pyramid after 180 degree

--- Patterns/Half_pyramid_using_asterik_after_180.cpp
+++ Patterns/Half_pyramid_using_asterik_after_180.cpp
@@ -8,7 +8,7 @@ int main(){
     for (int i = row; i >=1; i--)
     {
         for (int j = 1; j <= i; j++){
-            cout<<j<<" " ;
+            cout<<" * ";
         }
         cout<<endl;
     }
