50fc67d39a3c6077d08d5b4164e6d1e2b1036686
Abdullah-Niaz <abdullahniaz423@gmail.com>
2023-04-06 22:37:52 +0500

    loop programs

---
+++ 3. Loops/For Loop/sum_of_numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int sum = 0;
+    for (int i = 1; i <= 10; ++i)
+    {
+        sum = sum + i;
+    }
+    cout << sum;
+    return 0;
+}
