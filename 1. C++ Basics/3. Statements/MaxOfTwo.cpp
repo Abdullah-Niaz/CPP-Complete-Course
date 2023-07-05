95b883493ad4e1f15c0f7c6074291a8d7eaaf6cb
Abdullah-Niaz <abdullahniaz423@gmail.com>
2023-07-05 12:52:33 +0500

    Optimized

---
+++ 1. C++ Basics/3. Statements/MaxOfTwo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter two numbers a & b Respectively: ";
+    cin>>a >> b;
+    cout<<"First NUmbers is : "<<a<<endl;
+    cout<<"Second NUmbers is : "<<b<<endl;
+    if(a>b){
+        cout<<"First Number is greater which is "<<a<<endl;
+    }
+    else{
+        cout<<"Second number is greater which is "<<b<<endl;
+    }
+
+return 0;
+}
