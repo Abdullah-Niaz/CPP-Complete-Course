8c10557ce8afdfb344c7943798a325431032c488
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-08-03 16:37:39 +0500

    intro to Char using Array

---
+++ 5. Strings/char_array.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char s[10] = "Abdullah";
+    cout<<"Value of Char Array is: "<<s;
+    
+    // For Loop
+    // for (int i = 0; i <=10; i++)
+    // {
+    //     cout<<s[i];
+    // }
+    
+    // For Each Loop 
+    // for(auto x:s){
+    //     cout<<x;
+    // }
+
+    
+    // While Loop
+    // int i = 0 ;
+    // while (s[i] > 0)
+    // {
+    //     cout<<s[i]<<endl;
+    //     i++;
+    // }
+    
+    // Do While Loop
+    // int i = 0;
+    // cout<<endl;
+    // do
+    // {
+    //     cout<<s[i];
+    //     i++;
+    // } while (s[i]>0);
+    
+};
