995f4a504ac8482e2ac7c981def5bab8066de1c9
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-08-17 23:29:04 +0500

    improvements

---
+++ OOP/OOP_Examples/BankAccount.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class BankAccount
+{
+private:
+    double balance;
+
+public:
+    void intial_Balance(double i_balance)
+    {
+        balance = i_balance;
+    }
+
+    void deposit(double amount)
+    {
+        if (amount > 0)
+        {
+            balance += amount;
+        }
+    }
+
+    void withdraw(double amount)
+    {
+        if (amount > 0 && amount <= balance)
+        {
+            balance -= amount;
+        }
+    }
+
+    double get_balance()
+    {
+        return balance;
+    }
+};
+
+int main()
+{
+    BankAccount ali;
+    ali.intial_Balance(500);
+    ali.deposit(500);
+    ali.withdraw(200);
+
+    cout << "Current balance: " << ali.get_balance() << endl;
+
+    return 0;
+}
