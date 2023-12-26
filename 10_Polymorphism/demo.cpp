216c1c1ca966d3547827c2407b702fb2d800a2bf
Abdullah-Niaz <abdullahniaz423@gmail.com>
2023-12-27 04:31:34 +0500

    polymorphism

---
+++ 10_Polymorphism/demo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+class Parent{
+    public:
+        void display(){
+            cout<<"Display Parent";
+        }
+};
+
+int main(){
+
+    return 0;
+}
