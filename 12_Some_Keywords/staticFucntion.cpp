792616483b0315163d8c779fc7be40df6425c229
Abdullah-Niaz <abdullahniaz423@gmail.com>
2023-12-28 03:43:18 +0500

    Static Functions

---
+++ 12_Some_Keywords/staticFucntion.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class Test
+{
+private:
+    int a, b;
+
+public:
+    static int count;
+
+    Test()
+    {
+        a = 10;
+        b = 20;
+        count++;
+    }
+    static int coutnadd(){
+        return count;
+    }
+};
+
+int Test::count = 0;
+
+int main()
+{
+    Test t1;
+    Test t2;
+    Test t3;
+
+    cout<<Test::coutnadd();
+    // cout << t1.count << endl;
+
+    // cout << t2.count << endl;
+    // cout << t3.count << endl;
+    return 0;
+}
