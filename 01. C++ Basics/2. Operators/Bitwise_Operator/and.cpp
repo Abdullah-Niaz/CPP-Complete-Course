f92a6fbce114ac9e444374c9343acc8cb6c7b574
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-01-17 21:29:45 +0500

    and operator

---
+++ 01. C++ Basics/2. Operators/Bitwise_Operator/and.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 12;
+    int b = 25; 
+    cout<<"Value of a: "<<byte(a)<<enld;
+    cout<<"Value of a: "<<byte(b)<<enld;
+    cout<<"Value of a & b: "<<(a & b)<<enld;
+    return 0;
+}
